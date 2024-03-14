8f0afb98ef68928f9b202b1d56f89a75c79ce91a
Jessica <jessicakressner@oakland.edu>
2024-03-13 20:58:56 -0400

    Modified home page and added some code to make api call in js file

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-fetch("https://api.spoonacular.com/recipes/complexSearch")
-  .then((response) => response.json())
-  .then((recipes) => console.log(recipes));
+// import statements
+import $ from "jquery";
+
+// API key
+const apiKey = b6d5b4dac4414fb09698cc9810300e29;
+// cusine input
+const cusine = document.getElementById("cusine");
+// dish input
+const dish = document.getElementById("dish");
+// ingredients input
+const ingredients = document.getElementById("ingredients");
+$(document).ready(function () {
+  $("#recipes").click(function () {
+    const cusine = $("#cusine").val();
+    const dish = $("#dish").val();
+    const ingredients = $("ingredients").val();
+
+    // api url
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cusine=${cusine}&query=${dish}&ingredients=${ingredients}`;
+    // request
+    request.onreadystatechange = function () {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.send();
+  });
+});
+
+// fetch data
+//fetch(url)
+//.then((response) => response.json())
+//.then((data) => (recipes.textContext = data));
